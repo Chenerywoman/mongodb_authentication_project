802fe40a3ecab52ed56863011bb2149b84f1f7e8
Chenerywoman <rachelmchenery@hotmail.com>
2021-01-07 20:36:00 +0000

    creating helpers file & extracting cookie creation to createCookie function

---
+++ helpers.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+
+const User = require('./models/userModel');
+
+exports.createCookie = (userId, response) => {
+
+    const token = jwt.sign({id: userId}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
+        
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+
+    response.cookie('jwt', token, cookieOptions)
+}
